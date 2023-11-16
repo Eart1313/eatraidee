836d5a15598d8ee29a1a9bb39465d9dbe221fc8f
Eart <thepray1313@gmail.com>
2023-11-16 22:53:31 +0700

    fix:router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import App from "./App.jsx";
-import "./App.css";
 import Homepage from "./pages/homepage.jsx";
 import DonatePage from "./pages/DonatePage.jsx";
+import "./App.css";
 import "./index.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> },
-  { path: "/donate", element: <DonatePage /> },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/donate" element={<DonatePage />} />
+    </Router>
   </React.StrictMode>
 );
